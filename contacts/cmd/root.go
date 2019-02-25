fb8713327e0eef22554769be54739e1a43c4a3fe
Matthew Bell <matt@mbell.me>
2019-02-25 15:54:19 -0700

    Add more to contacts svc

--- contacts/cmd/root.go
+++ contacts/cmd/root.go
@@ -5,21 +5,39 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "bittr-contacts",
 	Short: "bittr contacts microservice",
 	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(cmd.Flags().Lookup("port").Value)
 	},
 }
 
 var (
-	port = "8080"
+	port   = "8080"
+	pgURL  = "localhost"
+	pgPort = "5432"
+	pgUser = "root"
+	pgPass = "root"
+	pgName = "db"
 )
 
 func init() {
 	rootCmd.PersistentFlags().StringVarP(&port, "port", "p", "PORT", "Port to listen on (default: 8080)")
+	rootCmd.PersistentFlags().StringVar(&pgURL, "pgurl", "POSTGRES_URL", "URL of postgresql server")
+	rootCmd.PersistentFlags().StringVar(&pgPort, "pgport", "POSTGRES_PORT", "port to connect to postgresql server")
+	rootCmd.PersistentFlags().StringVar(&pgUser, "pguser", "POSTGRES_USER", "Username for postgresql server")
+	rootCmd.PersistentFlags().StringVar(&pgPass, "pgpass", "POSTGRES_DB", "Password for postgresql server")
+	rootCmd.PersistentFlags().StringVar(&pgName, "pgname", "POSTGRES_NAME", "Name of postgresql database")
+	viper.BindPFlag("port", rootCmd.Flags().Lookup("port"))
+	viper.BindPFlag("postgres_url", rootCmd.Flags().Lookup("pgurl"))
+	viper.BindPFlag("postgres_port", rootCmd.Flags().Lookup("pgport"))
+	viper.BindPFlag("postgres_user", rootCmd.Flags().Lookup("pguser"))
+	viper.BindPFlag("postgres_pass", rootCmd.Flags().Lookup("pgpass"))
+	viper.BindPFlag("postgres_name", rootCmd.Flags().Lookup("pgname"))
 }
 
 // Execute executes the program
--- contacts/go.mod
+++ contacts/go.mod
@@ -1,7 +1,15 @@
 module github.com/mbellgb/bittr/contacts
 
 require (
+	github.com/coreos/etcd v3.3.12+incompatible // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/spf13/afero v1.2.1 // indirect
 	github.com/spf13/cobra v0.0.3
-	github.com/spf13/viper v1.3.1 // indirect
+	github.com/spf13/viper v1.3.1
+	github.com/stretchr/objx v0.1.1 // indirect
+	github.com/stretchr/testify v1.3.0 // indirect
+	github.com/ugorji/go/codec v0.0.0-20190204201341-e444a5086c43 // indirect
+	golang.org/x/crypto v0.0.0-20190222235706-ffb98f73852f // indirect
+	golang.org/x/sys v0.0.0-20190222171317-cd391775e71e // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
